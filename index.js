269345e855cfef125a48aa0981b7f1b0e2a409a7
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-29 19:44:42 +0300

    create a middleware for roles router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const db = require("./db/index");
+const morgan = require("morgan")
 
 const app = express();
 
@@ -8,7 +9,14 @@ const app = express();
 /// app Level Middleware
 app.use(express.json());
 
+//// craete a middleware for roles router
+const rolesRouter = require("./routers/routes/roles");
+app.use(rolesRouter);
+
+
+
 PORT = process.env.PORT || 4000;
+
 
 
 
